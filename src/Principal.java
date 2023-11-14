86d42afa1ca0c58bcec615288bf055b25b172007
MinaBoudj <aminaboudjedir5@gmail.com>
2023-11-14 21:30:43 +0100

    création des classes et des methode de chaque classe non implémenter

---
+++ src/Principal.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Principal{
+    public static void main(String[] args){
+
+        //initialisation d'un objet Quadtree à partir d'un fichier PGM
+        Quadtree quadtree = new Quadtree("amina/ASD3/PROJET/images/boat.pgm");
+
+        //affichage du menu interactif
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+
+        do{
+            System.out.println("1. Choisir une autre image PGM");
+            System.out.println("2. Appliquer la compression Lambda");
+            System.out.println("3. Appliquer la compression Rho");
+            System.out.println("4. Générer le fichier PGM");
+            System.out.println("5. Afficher les statistiques de compression ");
+            System.out.println("6. QUITTER");
+
+            System.out.println("Choix : ");
+            choice = scanner.nextInt();
+
+            switch (choice){
+                case 1: System.out.println("Entrer le chemin vers une autre image : ");
+                        String newImagePath = scanner.next();
+                        quadtree = new Quadtree(newImagePath);
+                        break;
+
+                case 2: quadtree.compressLambda();
+                        break;
+
+                case 3: System.out.println("Entrer la valeur du pourcentage de compression Rho : ");
+                        int percentage = scanner.nextInt();
+                        quadtree.compressRho(percentage);
+                        break;
+
+                case 4: System.out.println("Entrer le chemin de sortie du fichier PGM : ");
+                        String outputFilePath = scanner.next();
+                        quadtree.toPGM(outputFilePath);
+                        break;
+
+                case 5: //le nombre de noeuds du quadtree initial
+                        //le nombre de noeuds du quadtree résultant après la compression choisis
+                        //le taux de compression réalisé
+                        break;
+                default : System.out.println("ERREUR !");
+            }
+
+        }while(choice !=0);
+    }
+
+
+
+}
---
+++ src/Quadtree.java
@@ -0,0 +1,45 @@
+public class Quadtree{
+    private Noeud racine;
+
+    private static class Noeud{
+        int luminosité;
+        Noeud[] fils;
+
+        Noeud(int luminosité){
+            this.luminosité = luminosité;
+            this.fils = new Noeud[4]; //4 fils pour chaque noeud
+        }
+    }
+    //constructeur 
+    public Quadtree(String FilePath){
+        //charge l'image PGM et construit le quadtree correspondant
+        this.racine = null;
+    }
+
+    //la méthode qui donne la représentation textuelle du quadtree sous forme parenthésée (comme vu en TD2) où
+    // chaque valeur de luminosite sera representee par sa valeur decimale
+    public String toString(){
+        return " ";
+    }
+
+    //Methode qui génère à l'endroit path un fichier PGM qui correspond au Quadtree
+    public void toPGM(String path){
+
+    }
+
+    //Methode qui compresse le Quadtree selon la premiere technique 2.3.1
+    public void compressLambda(){
+
+    }
+
+    //Methode qui compresse le Quadtree selon la seconde technique 2.3.2
+    public void compressRho(int percentage){
+
+    }
+
+
+
+
+
+    
+}
